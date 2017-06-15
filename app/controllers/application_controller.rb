11cf239c86dc910a2d635004e5251af7025fa395
Las niñas <Las nias>
2017-06-15 13:32:04 -0500

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #protected
+  protected
 
-  #def configure_permitted_parameters
-    #devise_parameter_sanitizer.for(:sing_up) { |u| u.permit(:email, :password, :password_confirmation, :phone)}
-    #end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :phone])
+
+    end
   end
 
